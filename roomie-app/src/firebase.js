d743e36043c0ba3d8353c0fc87d3437629cb527e
Emmie Ohnuki <emaohn@gmail.com>
2021-02-19 22:23:52 -0500

    Setup firebase and context files

---
+++ roomie-app/src/firebase.js
@@ -0,0 +1,20 @@
+import app from "firebase/app"
+import 'firebase/auth';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDyMVuTZG5O-L_wWF_zFCuUfZVeX-m2eXY",
+    authDomain: "hackbeanpot-roomie-app.firebaseapp.com",
+    projectId: "hackbeanpot-roomie-app",
+    storageBucket: "hackbeanpot-roomie-app.appspot.com",
+    messagingSenderId: "998032024340",
+    appId: "1:998032024340:web:cdbc8e12da9f7855011801",
+    measurementId: "G-EHH1EP9JQV"
+};
+
+export default class Firebase {
+    constructor() {
+        app.initializeApp(firebaseConfig);
+    }
+}
+
