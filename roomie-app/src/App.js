310ae0eaf5cbc1d688ce86f8f46f1f0ea365cbd4
Solvence <anthonyhmu@gmail.com>
2021-02-20 02:20:36 -0500

    started auth

--- roomie-app/src/App.js
+++ roomie-app/src/App.js
@@ -2,40 +2,67 @@ import logo from './logo.svg';
 import './App.css';
 // import { FirebaseContext } from './firebase'
 import firebase from './firebase/firebase'
+import "firebase/auth";
+import {
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer,
+  IfFirebaseAuthed,
+  IfFirebaseAuthedAnd
+} from "@react-firebase/auth";
 import React from 'react'
 
 function App() {
-    const ref = firebase.firestore().collection("users")
-    
-    ref.onSnapshot((querySnapshot) => {
-      const users = []
-      querySnapshot.forEach((doc) => {
-        users.push(doc.data())
+  const ref = firebase.firestore().collection("users")
 
-        doc.data().house[0].get().then(data => {
-          console.log(data.data())
-        })
+  ref.onSnapshot((querySnapshot) => {
+    const users = []
+    querySnapshot.forEach((doc) => {
+      users.push(doc.data())
+
+      doc.data().house[0].get().then(data => {
+        console.log(data.data())
       })
     })
+  })
 
-    return (
+  return (
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <FirebaseAuthProvider firebase={firebase} {...config}>
+          {
+            // my app code
+          }
+        </FirebaseAuthProvider>
+
+        <FirebaseAuthConsumer>
+          {({ isSignedIn, user, providerId }) => {
+            return (
+              <pre style={{ height: 300, overflow: "auto" }}>
+                {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
+              </pre>
+            );
+          }}
+        </FirebaseAuthConsumer>
+
+        <IfFirebaseAuthedAnd
+          filter={({ providerId, user }) => {
+            if(!user.email){return false;}
+            return (
+              providerId !== "anonymous" &&
+              user.email.indexOf("@companyname.com") > -1
+            );
+          }}
           >
-            Learn React
-          </a>
-        </header>
+          {({ isSignedIn, user, providerId }) => {
+            return (
+              <div> Hello </div>
+            );
+          }}
+        </IfFirebaseAuthedAnd>
       </div>
-    );
+
+    
+  );
 }
 
 export default App;
