6b94e96a25ed68f9a899fb7ae94c427dcac84ef3
Solvence <anthonyhmu@gmail.com>
2021-02-20 23:33:25 -0500

    auth complete for email/google

--- roomie-app/src/App.js
+++ roomie-app/src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 // import { FirebaseContext } from './firebase'
 import firebase from './firebase/firebase'
-import "firebase/auth";
-import {
-  FirebaseAuthProvider,
-  FirebaseAuthConsumer,
-  IfFirebaseAuthed,
-  IfFirebaseAuthedAnd
-} from "@react-firebase/auth";
-import React from 'react'
+
+import React, { useContext } from "react";
+import { Router } from "@reach/router";
+import SignIn from "./Components/SignIn";
+import SignUp from "./Components/SignUp";
+import Application from "./Components/Application";
+import UserProvider from "./providers/UserProvider";
+import ProfilePage from "./Components/ProfilePage";
+import { UserContext } from "./providers/UserProvider";
 
 var config = {
   apiKey: "AIzaSyDyMVuTZG5O-L_wWF_zFCuUfZVeX-m2eXY",
@@ -24,73 +24,10 @@ var config = {
 function App() {
   const ref = firebase.firestore().collection("users")
 
-  ref.onSnapshot((querySnapshot) => {
-    const users = []
-    querySnapshot.forEach((doc) => {
-      users.push(doc.data())
-
-      doc.data().house[0].get().then(data => {
-        console.log(data.data())
-      })
-    })
-  })
-
   return (
-
-      <div className="App">
-        <FirebaseAuthProvider {...config} firebase={firebase}>
-      <div>
-        <button
-          onClick={() => {
-            const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-            firebase.auth().signInWithPopup(googleAuthProvider);
-          }}
-        >
-          Sign In with Google
-        </button>
-        <button
-          data-testid="signin-anon"
-          onClick={() => {
-            firebase.auth().signInAnonymously();
-          }}
-        >
-          Sign In Anonymously
-        </button>
-        <button
-          onClick={() => {
-            firebase.auth().signOut();
-          }}
-        >
-          Sign Out
-        </button>
-        <FirebaseAuthConsumer>
-          {({ isSignedIn, user, providerId }) => {
-            return (
-              <pre style={{ height: 300, overflow: "auto" }}>
-                {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
-              </pre>
-            );
-          }}
-        </FirebaseAuthConsumer>
-        <div>
-          <IfFirebaseAuthed>
-            {() => {
-              return <div>You are authenticated</div>;
-            }}
-          </IfFirebaseAuthed>
-          <IfFirebaseAuthedAnd
-            filter={({ providerId }) => providerId !== "anonymous"}
-          >
-            {({ providerId }) => {
-              return <div>You are authenticated with {providerId}</div>;
-            }}
-          </IfFirebaseAuthedAnd>
-        </div>
-      </div>
-    </FirebaseAuthProvider>
-      </div>
-
-    
+    <UserProvider>
+      <Application />
+    </UserProvider>
   );
 }
 
